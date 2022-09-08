0248c668fc949c7da7177cd4e368be9067f34ff4
chenliu <accpliuchen@gmail.com>
2022-09-08 01:28:35 -0400

    updated html

--- app.py
+++ app.py
@@ -31,7 +31,6 @@
 
 from flask import Flask, redirect, url_for, request, abort
 
-from views.index import index_blueprint
 from news.index import news_blueprint
 from institution.index import institution_blueprint
 from users.index import users_blueprint
@@ -58,7 +57,6 @@ application.config['MYSQL_DATABASE_DB'] = 'roytuts'
 application.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # mysql.init_app(application)
 
-application.register_blueprint(index_blueprint)
 application.register_blueprint(news_blueprint)
 application.register_blueprint(users_blueprint)
 application.register_blueprint(institution_blueprint)
