81e8399638e54f984ba16913d98d6db6ce09dd19
chenliu <accpliuchen@gmail.com>
2022-09-08 01:45:45 -0400

    updated html

--- app.py
+++ app.py
@@ -1,67 +1,29 @@
-# from datetime import datetime
-# from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def index():
-#    print('Request for index page received')
-#    return render_template('index.html')
-#
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-#
-# @app.route('/hello', methods=['POST'])
-# def hello():
-#    name = request.form.get('name')
-#
-#    if name:
-#        print('Request for hello page received with name=%s' % name)
-#        return render_template('hello.html', name = name)
-#    else:
-#        print('Request for hello page received with no name or blank name -- redirecting')
-#        return redirect(url_for('index'))
-#
-#
-# if __name__ == '__main__':
-#    app.run()
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
 
 
-from flask import Flask, redirect, url_for, request, abort
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-from views.index import index_blueprint
-from news.index import news_blueprint
-from institution.index import institution_blueprint
-from users.index import users_blueprint
-from protocol.index import protocol_blueprint
-from patient.index import patient_blueprint
-from match.index import match_blueprint
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# from flaskext.mysql import MySQL
-# # from config import db_conf
-# # import config
-from flask_cors import CORS
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-application = Flask(__name__)
-CORS(application)
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
-#mysql = MySQL()
 
-application.config['SECRET_KEY'] = 'secret-key'
-application.config['CORS_HEADERS'] = 'Content-Type'
-
-application.config['MYSQL_DATABASE_USER'] = 'root'
-application.config['MYSQL_DATABASE_PASSWORD'] = '123456'
-application.config['MYSQL_DATABASE_DB'] = 'roytuts'
-application.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(application)
-
-application.register_blueprint(index_blueprint)
-application.register_blueprint(news_blueprint)
-application.register_blueprint(users_blueprint)
-application.register_blueprint(institution_blueprint)
-application.register_blueprint(protocol_blueprint)
-application.register_blueprint(patient_blueprint)
-application.register_blueprint(match_blueprint)
+if __name__ == '__main__':
+   app.run()
--- news/index.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-news_blueprint = Blueprint('news', __name__)
-
-@news_blueprint.route("/news")
-def index():
-    return "Hello news!"
